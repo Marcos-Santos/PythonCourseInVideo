d0022e0f89a9dbc42f25ea626cc76413773a43db
Marcos-Santos <marcos_alexandre@msn.com>
2024-08-12 01:08:23 -0300

    Add Funcionalidade nome do usuario

--- app.py
+++ app.py
@@ -1,1 +1,5 @@
-print("Hello World code in Python by Marcos Alexandre")
+
+# App exibe Hello World mais nome digitado
+
+nome = input ("Por favor escreva seu nome: ")
+print("Hello World code in Python por",nome)
